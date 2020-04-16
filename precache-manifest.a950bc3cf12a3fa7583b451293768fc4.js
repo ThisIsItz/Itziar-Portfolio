d8d49928f1ef34fce7d0cb44bdbc7d4a88682520
ThisIsItz <itziarurbieta94@gmail.com>
2020-04-16 18:50:03 +0100

    Updates

---
+++ precache-manifest.a950bc3cf12a3fa7583b451293768fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3ebdf8ae163973ca2d195139eb8949",
+    "url": "/Itziar-Portfolio/index.html"
+  },
+  {
+    "revision": "135bcdadae2aa9e7cd43",
+    "url": "/Itziar-Portfolio/static/js/2.e5c0d768.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Itziar-Portfolio/static/js/2.e5c0d768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da244011e4967ec0b7d",
+    "url": "/Itziar-Portfolio/static/js/main.41510d60.chunk.js"
+  },
+  {
+    "revision": "bc7221846f6c73d783bb",
+    "url": "/Itziar-Portfolio/static/js/runtime-main.206ea36e.js"
+  }
+]);
